0ac1e072ccc0feb1e87e8425bc16f2284a993c4e
Malcolm <supermowkitty@gmail.com>
2024-01-12 17:38:51 -0800

    GPS Sets Default Position

--- test_app/lib/pages/routeDisplay.dart
+++ test_app/lib/pages/routeDisplay.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:test_app/objects/Route.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 
 class RouteDisplay extends StatefulWidget {
   const RouteDisplay({super.key, required this.route});
@@ -13,6 +15,63 @@ class RouteDisplay extends StatefulWidget {
 }
 
 class _RouteDisplayState extends State<RouteDisplay> {
+
+  Position? _currPos;
+  final mapController = MapController();
+  void _getCurrentPos() async {
+    Position position = await _determinePosition();
+    setState(() {
+      _currPos = position;
+    });
+    mapController.move(new LatLng(position.latitude , position.longitude), 1000);
+  }
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    _getCurrentPos();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,12 +87,12 @@ class _RouteDisplayState extends State<RouteDisplay> {
         actions: [
           IconButton(
             icon: const Icon(Icons.list),
-            onPressed: () {},
+            onPressed: _getCurrentPos,
           )
         ],
       ),
       body: FlutterMap(
-        mapController: MapController(),
+        mapController: mapController,
         options: const MapOptions(
           initialCenter: LatLng(51.509364, -0.128928),
           maxZoom: 10,
