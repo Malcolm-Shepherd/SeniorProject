d0cbcab6780eac3d3e36ba96ef689f0f23156f11
Danielson77789 <dpeterson8@ewu.edu>
2024-03-15 11:30:54 -0700

    Pushing up changes to scheduler to merge into dev

--- test_app/lib/pages/taskHandling.dart
+++ test_app/lib/pages/taskHandling.dart
@@ -4,6 +4,8 @@ import 'package:flutter/services.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'dart:convert';
 
+import 'package:test_app/pages/jobInfo.dart';
+
 Map<DateTime, List<Event>> events = {};
 
 Future<List<Map<String, dynamic>>> loadMockData() async {
@@ -97,27 +99,50 @@ Widget ListViewBuilder2(List<Map<String, dynamic>>? data) {
         itemCount: data.length,
         itemBuilder: (context, index) {
           if (data![index]["date"].toString() ==
-              '${_focusedDay?.month}/${_focusedDay?.day}/${_focusedDay?.year}') {
+              '${_focusedDay.month}/${_focusedDay.day}/${_focusedDay.year}') {
             return Container(
               margin: const EdgeInsets.all(10),
               padding: const EdgeInsets.all(20),
               decoration: const BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.all(Radius.circular(10))),
+                color: Colors.grey,
+              ),
               child: Column(
                 children: [
-                  Text(
-                    data[index]["job_title"].toString(),
-                    style: const TextStyle(fontSize: 30),
+                  ListTile(
+                    title: Text("Job Title"),
+                    subtitle: Text(
+                      data[index]["job_title"].toString(),
+                      style: const TextStyle(fontSize: 30),
+                    ),
                   ),
-                  const Text("Date:"),
-                  Text(data[index]["date"].toString()),
-                  const Text("Responsibilities:"),
-                  Text(data[index]["responsibilities"].toString()),
-                  const Text("Clearances:"),
-                  Text(data[index]["clearances"].toString()),
-                  const Text("Requirements:"),
-                  Text(data[index]["requirements"].toString()),
+                  ListTile(
+                    title: Text("Date:"),
+                    subtitle: Text(data[index]["date"].toString()),
+                  ),
+                  ListTile(
+                    title: const Text("Clearances:"),
+                  ),
+                  Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          color: Colors.blueAccent),
+                      child: Text(data[index]["clearances"].toString())),
+                  TextButton(
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                JobDisplay()), // Use MaterialPageRoute to navigate to a new screen
+                      );
+                    },
+                    child: const Text('Click to open job'),
+                  )
                 ],
               ),
             );
